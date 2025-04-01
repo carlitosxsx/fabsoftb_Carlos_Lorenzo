aa3c719942500a34148067daa3e2b37d213dcda9
carlitosxsx <carlosaugustobs9@gmail.com>
2025-04-01 20:27:36 -0300

    project init

---
+++ projfabsoftmp/src/main/java/br/univille/projfabsoftmp/ProjfabsoftmpApplication.java
@@ -0,0 +1,13 @@
+package br.univille.projfabsoftmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjfabsoftmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjfabsoftmpApplication.class, args);
+	}
+
+}
---
+++ projfabsoftmp/src/test/java/br/univille/projfabsoftmp/ProjfabsoftmpApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.projfabsoftmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjfabsoftmpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
