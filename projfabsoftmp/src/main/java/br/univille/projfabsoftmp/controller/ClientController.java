2fd14e6124de101b1a98a4abcd715ecea83f7396
CARLOS AUGUSTO BRAZÃO DA SILVA <carlos.silva23@univille.br>
2025-04-28 21:20:53 -0300

    new api's rest

--- projfabsoftmp/src/main/java/br/univille/projfabsoftmp/controller/ClientController.java
+++ projfabsoftmp/src/main/java/br/univille/projfabsoftmp/controller/ClientController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +15,7 @@ import br.univille.projfabsoftmp.entity.Client;
 import br.univille.projfabsoftmp.service.ClientService;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
@@ -40,6 +43,34 @@ public class ClientController {
 
         }
         return ResponseEntity.badRequest().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Client> putClient(@PathVariable long id, @RequestBody Client client){
+        if(id == 0 || client == null){
+            return ResponseEntity.badRequest().build();
+        }
+        var clientExistente = service.getById(id);
+        if(clientExistente == null){
+            return ResponseEntity.notFound().build();
+        }
+        clientExistente.setName(client.getName());
+        service.save(clientExistente);
+        return new ResponseEntity<Client>(clientExistente, HttpStatus.OK);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Client> deleteClient(@PathVariable long id){
+        if(id <= 0){
+            return ResponseEntity.badRequest().build();
+        }
+        var clientExistente = service.getById(id);
+        if(clientExistente == null){
+            return ResponseEntity.notFound().build();
+        }
+        service.delete(id);
+        return new ResponseEntity<Client>(clientExistente, HttpStatus.OK);
     }
 
 }
--- projfabsoftmp/src/main/java/br/univille/projfabsoftmp/entity/Client.java
+++ projfabsoftmp/src/main/java/br/univille/projfabsoftmp/entity/Client.java
@@ -1,6 +1,10 @@
 package br.univille.projfabsoftmp.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Client {
@@ -21,5 +25,12 @@ public class Client {
 
     public String getName() {
         return name;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
     }
 }
