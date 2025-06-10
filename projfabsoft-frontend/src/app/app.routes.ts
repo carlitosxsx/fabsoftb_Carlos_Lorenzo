b1a6ea0ecc741e72be7ab454ae49df631c6f0165
Lorenzo <139818716+lorenzoponcio@users.noreply.github.com>
2025-06-10 00:54:21 +0000

    Nova tela de inclusão de clientes

--- projfabsoft-frontend/src/app/app.routes.ts
+++ projfabsoft-frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { EmployeeComponent } from './employee/employee.component';
+import { FormEmployeeComponent } from './form-employee/form-employee.component';
 
 export const routes: Routes = [
-    { path: 'employees', component: EmployeeComponent}
+    { path: 'employees', component: EmployeeComponent},
+    { path: 'employees/novo', component: FormEmployeeComponent}
 ];
--- projfabsoft-frontend/src/app/employee/employee.component.ts
+++ projfabsoft-frontend/src/app/employee/employee.component.ts
@@ -3,10 +3,11 @@ import { Employee } from '../model/employee';
 import { EmployeeService } from '../service/employee.service';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-employee',
-  imports: [HttpClientModule, CommonModule],
+  imports: [HttpClientModule, CommonModule, RouterLink],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css',
   providers: [EmployeeService]
